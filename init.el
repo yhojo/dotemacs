e3fde6ca070d84b2e6cb0444a759d19e1af61b12
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2012-06-27 14:07:07 +0900

    画面サイズを設定など。

--- init.el
+++ init.el
@@ -3,6 +3,24 @@
 (setq user-full-name "Yasuo Hojo")
 (setq user-email-address "yhojo@pp.iij4u.or.jp")
 
+;; Emacs 設定ディレクトリを設定。Emacs 22以下用
+;; Emacs 23.1 以上では user-emacs-directory 変数が用意されているのでそれを利用
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory (expand-file-name "~/.emacs.d/")))
+
+;; 引数を load-path へ追加
+;; normal-top-level-add-subdirs-to-load-path はディレクトリ中の中で
+;; [A-Za-z] で開始する物だけ追加するので、追加したくない物は . や _ を先頭に付与しておけばロードしない
+;; dolist は Emacs 21 から標準関数なので積極的に利用して良い
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; Common Lisp互換ライブラリの読み込み。
 (require 'cl)
 
 ;; 言語を日本語にする
@@ -12,7 +30,20 @@
 
 ;; 環境に合わせた初期化を実行する。
 (cond ((eq window-system 'ns)
-       ;; MacOS Cocoa Emacs
+       ;; default screen size
+       (setq default-frame-alist
+	     (append (list
+		      '(width . 60) ;; TODO: 何故か、Cocoa Emacsでは幅が全角ベース
+		      '(height . 37)
+		      '(top . 0)
+		      '(left . 0))
+		     default-frame-alist))
+       ;; kill-ringはテキスト属性を保存しない
+       (defadvice kill-new (around my-kill-ring-disable-text-property activate)
+	 (let ((new (ad-get-arg 0)))
+	   (set-text-properties 0 (length new) nil new)
+	   ad-do-it))
+       ;; MacOS Cocoa Emacs font settings
        (set-face-attribute
 	'default nil
 	:family "M+1VM+IPAG circle"
@@ -33,8 +64,7 @@
        (mac-input-method-mode)
        ))
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emu")
+(add-to-load-path "site-lisp")
 
 (load-library "pukiwiki-mode")
 (setq pukiwiki-process-timeout 300)
