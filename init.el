c539f1f7eef8c2c58aba99f4a7fb8ab5fc265899
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2012-06-27 12:20:57 +0900

    タイトル挿入スクリプトを追加

--- init.el
+++ init.el
@@ -42,3 +42,13 @@
       '(("MyWiki" "http://10.8.9.1/~yhojo/wiki/index.php")
       ))
 
+ ;;
+ ;; タイトル挿入スクリプト
+ ;;
+ (defun insert-title ()
+   "Insert Pukiwiki Style log title line"
+   (interactive)
+   (let ((today (format-time-string "%Y-%m-%d" (current-time)))
+         (title (read-string "Title: " "")))
+     (insert "* " title " - " today)))
+ (global-set-key "\C-x\C-^" 'insert-title)
