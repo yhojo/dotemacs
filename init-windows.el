513fc5288e13fb61578d002abd18d3aaff9209e1
Yasuo Hojo <yhojo@pp.iij4u.or.jp>
2013-06-18 20:11:58 +0900

    Windows迺ｰ蠅・〒縺ｮ繝舌ャ繧ｯ繧ｰ繝ｩ繧ｦ繝ｳ繝峨き繝ｩ繝ｼ繧帝ｻ偵↓螟画峩縺励◆縲・

--- init-windows.el
+++ init-windows.el
@@ -1,5 +1,15 @@
 (cond ((eq system-type 'windows-nt)
        (menu-bar-mode -1)
        (tool-bar-mode -1)
+       (setq default-frame-alist
+	     (append '((foreground-color     . "white") ;; 文字色
+		       (background-color     . "black") ;; 背景色
+		       (cursor-color         . "red"  ) ;; カーソル色
+		       ) default-frame-alist))
+       (setq initial-frame-alist default-frame-alist)
        (w32-ime-initialize)
-       (setq default-input-method "W32-IME")))
+       (setq default-input-method "W32-IME")
+       (set-default-font "M+1VM+IPAG circle-10")
+       (set-fontset-font (frame-parameter nil 'font)
+			 'japanese-jisx0208
+			 '("M+1VM+IPAG circle" . "unicode-bmp"))))
